4c0b886149cef59a5dd9f4b495b3b8ba01d1777a
safasfeh <aalsafasfeh@ttu.edu.jo>
2026-03-17 11:50:03 -0500

    simulator.py

--- simulator.py
+++ simulator.py
@@ -1,27 +1,28 @@
 import numpy as np
 
+
 def simulate_ball_mill(speed, filling, feed_rate, solids, bond_work_index, cyclone_pressure):
     power = (
         500
-        + 8 * speed
-        + 5 * filling
-        + 6 * feed_rate
-        + 2 * solids
-        + 10 * bond_work_index
-        + 1.5 * cyclone_pressure
-        + np.random.normal(0, 15)
+        + 7.5 * speed
+        + 4.5 * filling
+        + 5.5 * feed_rate
+        + 1.8 * solids
+        + 11.0 * bond_work_index
+        + 1.2 * cyclone_pressure
+        + np.random.normal(0, 12)
     )
 
     p80 = (
-        250
-        - 0.8 * speed
-        - 0.5 * filling
-        - 0.4 * solids
-        + 0.3 * feed_rate
-        + 1.2 * bond_work_index
-        + np.random.normal(0, 5)
+        260
+        - 1.0 * speed
+        - 0.6 * filling
+        - 0.45 * solids
+        + 0.2 * feed_rate
+        + 1.0 * bond_work_index
+        + np.random.normal(0, 4)
     )
 
-    throughput = feed_rate * (1 + (speed - 70) / 200)
+    throughput = feed_rate * (1 + (speed - 70) / 180) + np.random.normal(0, 0.3)
 
     return power, p80, throughput
